a6d2884790fdfa109ed85e5c62a43442db5dfb7c
Sultan Syarif Ubaidillah Al Nashr <sultansyarif630@gmail.com>
2026-09-16 12:20:00 +0700

    rapikan tampilan nav Langkah 12: yang ke-4

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,13 @@
 <nav class="bg-slate-900 text-white px-4 py-3 flex gap-4">
     <span class="font-semibold">Simple POS</span>
-    <a href="{{ route('pos.create') }}" class="hover:underline">Kasir</a>
-    <a href="{{ route('transactions.index') }}" class="hover:underline">Transaksi</a>
+
+    <a href="{{ route('pos.create') }}"
+       class="hover:underline px-2 py-1 rounded {{ request()->routeIs('pos.create') ? 'bg-slate-700' : '' }}">
+        Kasir
+    </a>
+
+    <a href="{{ route('transactions.index') }}"
+       class="hover:underline px-2 py-1 rounded {{ request()->routeIs('transactions.index') ? 'bg-slate-700' : '' }}">
+        Transaksi
+    </a>
 </nav>
